6f060419c5e2b1417cddc8017658111b40786be9
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-15 00:57:19 +0530

    updated

--- app.js
+++ app.js
@@ -27,20 +27,14 @@ async function getuserdate(url) {
 
     } catch (error) {
         console.log("error found" + error);
-
     }
 }
-
-
 function updateProfile(data){
-
-    
-
     data1.innerText =`1) ${data.facts[0]}`
     data2.innerText =`2) ${data.facts[1]}`
     data3.innerText =`3) ${data.facts[2]}`
     data4.innerText =`4) ${data.facts[3]}`
-    data5.innerText =`5) ${data.facts[4]}`
-    
-}
+    data5.innerText =`5) ${data.facts[4]}`  
 
+    console.log(data);
+}
